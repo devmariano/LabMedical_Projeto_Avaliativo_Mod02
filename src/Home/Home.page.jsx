4cd623491eadbdad0d478384f9dfbf7f44fa3627
Alexandre Mariano <86934710+devmariano@users.noreply.github.com>
2023-07-15 22:03:20 -0300

    criando paginas de home e login

---
+++ src/Home/Home.page.jsx
@@ -0,0 +1,9 @@
+export const HomePage = () => {
+ 
+    return(
+      <>
+        <p>HomePage is Render</p>
+      </>
+    )
+}
+
---
+++ src/Login/Login.page.jsx
@@ -0,0 +1,10 @@
+import * as Styled from './Login.style';
+
+export const LoginPage = () => {
+
+    return(
+        <>
+        <p>LoginPage is Render</p>
+        </>
+    )
+}
