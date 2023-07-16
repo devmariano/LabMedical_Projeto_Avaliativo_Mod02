a5d2139dca1a6cf977043958e912e4cef9da3578
Alexandre Mariano <86934710+devmariano@users.noreply.github.com>
2023-07-15 22:27:32 -0300

    configurando arquivo de rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { LoginPage } from './pages/Login/Login.page';
+import { HomePage } from './pages/Home/Home.page';
+
 
 
 function App() {
   return (
-    <>
-      <p>App LabMedical is Render</p>
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<LoginPage/>}/>
+        <Route path='/home' element={<HomePage/>}/>
+        <Route path='*' element={<><p>Página não existe</p></>}/>
+      </Routes>
+    </Router>
   )
 }
 
