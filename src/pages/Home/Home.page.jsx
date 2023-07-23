8fb75bd6b2177148a48b046b05bf6e99b2ed2a28
Alexandre Mariano <86934710+devmariano@users.noreply.github.com>
2023-07-23 19:04:15 -0300

    atualizada listagem e search para localStorage

--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -1,27 +1,30 @@
 import React, { useState, useEffect } from "react";
-import { useMenu } from "../../contexts/menu/menu.context";
-import CardStatus from "../../components/CardStatus/CardStatus";
+import { Container, Row, Col } from 'react-bootstrap';
 import { FaUser, FaStethoscope, FaFileMedical } from 'react-icons/fa';
 import * as Styled from './Home.style';
+import { useMenu } from "../../contexts/menu/menu.context";
+import CardStatus from "../../components/CardStatus/CardStatus";
 import SearchBar from "../../components/searchBar/searchBar.component";
-import { Container, Row, Col } from 'react-bootstrap';
 import CardPatient from "../../components/CardPatient/CardPatient.component";
+import { PatientService } from "../../services/Patient/Patient.service";
 
-import patientData from '../../data/patientData.json';
 
 export const HomePage = () => {
   const { setTittle } = useMenu();
   const [searchValue, setSearchValue] = useState(""); // State para armazenar o search value
   const [filteredPatients, setFilteredPatients] = useState([]); // State que armazena os array de pacientes filtrados
+  const totalPatients = PatientService.getPatients();
 
   useEffect(() => {
     setTittle('ESTATISTICAS E INFORMAÇÕES');
-    // Filtra pacientes com base no valor da busca 
-    const filteredPatients = patientData.patients.filter(paciente =>
-      paciente.nome.toLowerCase().includes(searchValue.toLowerCase())
-    );
-    setFilteredPatients(filteredPatients);
+    // Busca os pacientes no serviço
+    const patients = PatientService.getPatients();
+    
+    // Filtra os pacientes com base no valor da busca
+    const filtered = patients.filter(patient => patient.nome.toLowerCase().includes(searchValue.toLowerCase()));
+    setFilteredPatients(filtered);
   }, [searchValue, setTittle]);
+
 
   return (
     <Styled.Dasboard>
@@ -29,7 +32,7 @@ export const HomePage = () => {
         <Styled.Title>Estatísticas do Sistema</Styled.Title>
         <Row>
           <Col>
-            <CardStatus title="Pacientes Cadastrados" value="100" color='#36d2b1' icon={<FaUser />} />
+            <CardStatus title="Pacientes Cadastrados" value={totalPatients.length} color='#36d2b1' icon={<FaUser />} />
           </Col>
           <Col>
             <CardStatus title="Consultas Cadastradas" value="50" color='#6674d2' icon={<FaStethoscope />} />
@@ -41,9 +44,9 @@ export const HomePage = () => {
         <Styled.Title>Informações Rápidas de Pacientes</Styled.Title>
         <SearchBar setValorBuscado={setSearchValue} />
         <Row>
-          {filteredPatients.map((paciente, index) => (
+          {filteredPatients.map((patient, index) => (
             <Col key={index} xs={12} sm={6} md={4} lg={3}>
-              <CardPatient paciente={paciente} />
+              <CardPatient paciente={patient} />
             </Col>
           ))}
         </Row>
