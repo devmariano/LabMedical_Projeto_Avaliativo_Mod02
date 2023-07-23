7fe200bc8d5f409539de6986df250cc9d046085b
Alexandre Mariano <86934710+devmariano@users.noreply.github.com>
2023-07-23 01:08:52 -0300

    ajuste cards e criacao da searchbar

--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -1,39 +1,37 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { useMenu } from "../../contexts/menu/menu.context";
 import CardStatus from "../../components/CardStatus/CardStatus";
 import { FaUser, FaStethoscope, FaFileMedical } from 'react-icons/fa';
-import * as Styled from './Home.style'
-
+import * as Styled from './Home.style';
+import SearchBar from "../../components/searchBar/searchBar.component";
+import { Row, Col } from 'react-bootstrap';
 
 export const HomePage = () => {
-
   const { setTittle } = useMenu();
-  
-  useEffect(() => { 
-    setTittle('ESTATISTICAS E INFORMAÇÕES');
-      }, []);
-    
-    return(
-    <Styled.Dasboard>
-    <div class="container">
-    <Styled.Title>Estatísticas do Sistema</Styled.Title>
-    <div class="row">
-    <div class="col">
-    <CardStatus title="Pacientes Cadastrados" value="100" icon={<FaUser />} />
-    </div>
-    <div class="col">
-    <CardStatus title="Consultas Cadastradas" value="50" icon={<FaStethoscope />} />
-    </div>
-    <div class="col">
-    <CardStatus title="Exames Cadastrados" value="75" icon={<FaFileMedical />} />
-    </div>
-    
-  </div>
-  
-</div>
-      </Styled.Dasboard>
-    )
-  
 
+  useEffect(() => {
+    setTittle('ESTATISTICAS E INFORMAÇÕES');
+  }, []);
+
+  return (
+    <Styled.Dasboard>
+      <div className="container">
+        <Styled.Title>Estatísticas do Sistema</Styled.Title>
+        <Row>
+          <Col>
+            <CardStatus title="Pacientes Cadastrados" value="100" color='#36d2b1' icon={<FaUser />} />
+          </Col>
+          <Col>
+            <CardStatus title="Consultas Cadastradas" value="50" color='#6674d2' icon={<FaStethoscope />} />
+          </Col>
+          <Col>
+            <CardStatus title="Exames Cadastrados" value="75" color='#e98b58' icon={<FaFileMedical />} />
+          </Col>
+        </Row>
+        <Styled.Title>Informações Rápidas de Pacientes</Styled.Title>
+        <SearchBar />
+      </div>
+    </Styled.Dasboard>
+  )
 }
 
