4d2bd3ee030bbac854066057dab7d27d01125d95
Alexandre Mariano <86934710+devmariano@users.noreply.github.com>
2023-08-01 20:41:07 -0300

    adição de animação de spinner de transição

--- src/components/Footer/Footer.component.jsx
+++ src/components/Footer/Footer.component.jsx
@@ -1,6 +1,6 @@
 
 import * as Styled from './Footer.style';
-
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
 export const Footer = () => {
   
@@ -12,6 +12,14 @@ export const Footer = () => {
             </Styled.FooterLeft>
     <Styled.FooterRight>
     <Styled.Name>feito por Alexandre Mariano</Styled.Name>
+    <Styled.IconsContainer>
+          <Styled.IconLink href="https://github.com/devmariano" target="_blank">
+            <FaGithub />
+          </Styled.IconLink>
+          <Styled.IconLink href="https://www.linkedin.com/in/alexandre-mariano-04103012b" target="_blank">
+            <FaLinkedin />
+          </Styled.IconLink>
+        </Styled.IconsContainer>
     <Styled.Img src="https://avatars.githubusercontent.com/u/86934710?v=4"/>
     </Styled.FooterRight>
   </Styled.Footer>
