ecba9d2762abeb41c9241d39c64bc092c2fa1110
Alexandre Mariano <86934710+devmariano@users.noreply.github.com>
2023-08-01 14:30:27 -0300

    estilização dos cards e pagina prontuario

--- src/pages/PatientRecord/PatientRecord.page.jsx
+++ src/pages/PatientRecord/PatientRecord.page.jsx
@@ -4,8 +4,10 @@ import { useParams } from 'react-router-dom';
 import { PatientService } from '../../services/Patient/Patient.service';
 import { AppointmentService } from '../../services/Patient/Patient.service';
 import { ExamService } from '../../services/Patient/Patient.service';
+import { FaUser } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import { useMenu } from "../../contexts/menu/menu.context";
+import { Col,  Row } from "react-bootstrap";
 
 
 export const PatientRecordPage = () => {
@@ -46,51 +48,78 @@ export const PatientRecordPage = () => {
   };
 
   return (
-    <div>
-      {/* Dados do Paciente */}
-      {patient && (
-        <div>
-          <h2>{patient.nome}</h2>
-          <p>Convênio: {patient.convenio}</p>
-          <p>Contato de Emergência: {patient.contatoEmergencia}</p>
-          <p>Alergias: {patient.alergias || 'Nenhuma'}</p>
-          <p>Cuidados Específicos: {patient.cuidadosEspecificos}</p>
-        </div>
-      )}
+    <Styled.PageWrapper>
+      <Styled.PageContainer>
+        {/* Dados do Paciente */}
+        {patient && (
+          <Styled.PatientCard>
+            <Styled.CardContent>
+              <Row>
+                <Col xs="auto">
+                  <Styled.IconWrapper>
+                    <FaUser />
+                  </Styled.IconWrapper>
+                </Col>
+                <Col>
+                  <Styled.PatientName>{patient.nome}</Styled.PatientName>
+                  <Styled.PatientDataRow>
+                    <Styled.PatientData>
+                      <Styled.PatientDataLabel>Convênio:</Styled.PatientDataLabel>
+                      <p>{patient.convenio}</p>
+                      <Styled.PatientDataLabel>Contato de Emergência:</Styled.PatientDataLabel>
+                      <p>{patient.contatoEmergencia}</p>
+                    </Styled.PatientData>
+                    <Styled.PatientData>
+                      <Styled.PatientDataLabel>Alergias:</Styled.PatientDataLabel>
+                      <p>{patient.alergias || "Nenhuma"}</p>
+                      <Styled.PatientDataLabel>Cuidados Específicos:</Styled.PatientDataLabel>
+                      <p>{patient.cuidadosEspecificos}</p>
+                    </Styled.PatientData>
+                  </Styled.PatientDataRow>
+                </Col>
+              </Row>
+            </Styled.CardContent>
+          </Styled.PatientCard>
+        )}
 
       {/* Lista de Consultas */}
       <h3>Consultas</h3>
       {appointments.map((appointment) => (
-        <div key={appointment.id}>
-          <h4>Consulta #{appointment.id}</h4>
-          <p>Motivo: {appointment.motivo}</p>
-          <p>Data: {appointment.data}</p>
-          <p>Hora: {appointment.hora}</p>
-          <p>Descrição do Problema: {appointment.descricaoProblema}</p>
-          <p>Medicação Receitada: {appointment.medicacaoReceitada}</p>
-          <p>Dosagem e Precauções: {appointment.dosagemPrecaucoes}</p>
-          <Link to={`/edit-appointment/${appointment.id}`}>
-            <button>Editar Consulta</button>
-          </Link>
-        </div>
+        <Styled.AppointmentCard key={appointment.id}>
+          <Styled.CardContent>
+            <Styled.CardTitle>Consulta:  {appointment.id}</Styled.CardTitle>
+            <p>Motivo: {appointment.motivo}</p>
+            <p>Data: {appointment.data}</p>
+            <p>Hora: {appointment.hora}</p>
+            <p>Descrição do Problema: {appointment.descricaoProblema}</p>
+            <p>Medicação Receitada: {appointment.medicacaoReceitada}</p>
+            <p>Dosagem e Precauções: {appointment.dosagemPrecaucoes}</p>
+            <Link to={`/edit-appointment/${appointment.id}`}>
+              <Styled.StyledButton>Editar Consulta</Styled.StyledButton>
+            </Link>
+          </Styled.CardContent>
+        </Styled.AppointmentCard>
       ))}
 
       {/* Lista de Exames */}
       <h3>Exames</h3>
       {exams.map((exam) => (
-        <div key={exam.id}>
-          <h4>Exame #{exam.id}</h4>
-          <p>Nome do exame: {exam.nomeExame}</p>
-          <p>Data: {exam.data}</p>
-          <p>Hora: {exam.hora}</p>
-          <p>Laboratório: {exam.laboratorio}</p>
-          <p>Tipo: {exam.tipo}</p>
-          <p>Resultado: {exam.resultado}</p>
-          <Link to={`/edit-exam/${exam.id}`}>
-            <button>Editar Exame</button>
-          </Link>
-        </div>
+        <Styled.ExamCard key={exam.id}>
+          <Styled.CardContent>
+            <Styled.CardTitle>Exame: {exam.id}</Styled.CardTitle>
+            <p>Nome do exame: {exam.nomeExame}</p>
+            <p>Data: {exam.data}</p>
+            <p>Hora: {exam.hora}</p>
+            <p>Laboratório: {exam.laboratorio}</p>
+            <p>Tipo: {exam.tipo}</p>
+            <p>Resultado: {exam.resultado}</p>
+            <Link to={`/edit-exam/${exam.id}`}>
+              <Styled.StyledButton>Editar Exame</Styled.StyledButton>
+            </Link>
+          </Styled.CardContent>
+        </Styled.ExamCard>
       ))}
-    </div>
+    </Styled.PageContainer>
+    </Styled.PageWrapper>
   );
 };
--- src/pages/PatientRecord/PatientRecord.style.jsx
+++ src/pages/PatientRecord/PatientRecord.style.jsx
@@ -1,1 +1,92 @@
-import styled  from 'styled-components';
+import styled from "styled-components";
+import { Card, Col, Container, Row } from "react-bootstrap";
+
+
+export const PageWrapper = styled.div`
+  background-color: #f6f8fb;
+  width: 100%;
+`;
+
+export const PageContainer = styled(Container)`
+  margin-bottom: 20px;
+  width: 85%;
+  padding-top: 5rem;
+`;
+
+export const PatientCard = styled(Card)`
+  margin-bottom: 20px;
+  border: none;
+  color: #145979;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+`;
+
+export const AppointmentCard = styled(Card)`
+  margin-bottom: 20px;
+  color: #116b66;
+  font-weight: 500;
+  border: none;
+  background-color:#16989112;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+`;
+
+export const ExamCard = styled(Card)`
+  margin-bottom: 20px;
+  color: #16587b;
+  font-weight: 500;
+  border: none;
+  background-color:#1a689220;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+`;
+
+
+export const CardTitle = styled(Card.Title)`
+  margin-bottom: 0;
+`;
+
+
+export const CardContent = styled(Card.Body)`
+  padding: 20px;
+`;
+
+export const IconWrapper = styled.div`
+  font-size: 4rem;
+  color: #549ABB;
+  margin-bottom: 1rem;
+  padding: 0%.5rem;
+`;
+
+export const PatientName = styled.h2`
+  padding-left: 1rem;
+  margin-top: 1rem;
+`;
+
+export const PatientDataRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 1rem;
+`;
+
+export const PatientData = styled.div`
+  flex: 1;
+  padding-left: 1rem;
+`;
+
+export const PatientDataLabel = styled.p`
+  font-weight: bold;
+  margin-bottom: 0.25rem;
+`;
+
+export const StyledButton = styled.button`
+
+  background-color: #f0a50ee2; 
+  color: white;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #f0a50e;
+  }
+`
